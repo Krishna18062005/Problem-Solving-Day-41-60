3c296bff43c1c0296dac8283880d6c49987a3340
Krishna K <krish1826s@gmail.com>
2024-05-31 06:30:49 +0530

    Update day_42.java

--- day_42.java
+++ day_42.java
@@ -20,3 +20,26 @@ public class Hello {
 
 	}
 }
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+		//Remove Characters of Next Word
+		Scanner sc=new Scanner(System.in);
+		String W=sc.nextLine(); 
+		String[] Ar=W.split(" ");
+		for(int i=0;i<Ar.length-1;i++){
+		    String ans="";
+		    for(char j:Ar[i].toCharArray()){
+		        
+		    if(j!=' '&&!Ar[i+1].contains(""+j))
+		    
+		    ans+=j;
+		}
+		if(ans!="")
+		    System.out.print(ans.trim()+" ");
+		}
+		System.out.print(Ar[Ar.length-1].trim());
+
+	}
+}
