ac98805714fcc9fc9a1d3c0eb5977f04d2a7e9e4
Krishna K <krish1826s@gmail.com>
2024-06-04 23:11:46 +0530

    Update day_47.java

--- day_47.java
+++ day_47.java
@@ -32,3 +32,52 @@ public class Hello {
 	    return c;
 	}
 }
+import java.util.*;
+public class Hello {
+    public static void main(String[] args) {
+		//Xth rank -Students
+		Scanner sc=new Scanner(System.in);
+		int N = sc.nextInt();
+		int X = sc.nextInt();
+		String[] name=new String[N];
+		int[] ma=new int[N];
+		for(int i=0;i<N;i++){
+		    name[i]=sc.next();
+		    ma[i]=sc.nextInt();
+		}
+		for(int i=0;i<N;i++){
+		    for(int j=i+1;j<N;j++){
+		        if(ma[i]<ma[j]){
+		            int t=ma[i];
+		            ma[i]=ma[j];
+		            ma[j]=t;
+		            String te=name[i];
+		            name[i]=name[j];
+		            name[j]=te;
+		        }
+		    }
+		}
+		HashMap<Integer,Integer> map=new HashMap<>();
+		int c=1;
+		int p=0;
+		for(int i:ma){
+		    if(map.containsKey(i)){
+		        map.put(i,p);
+		    }
+		    else{
+		        p=c;
+		        map.put(i,c);
+			    }c+=1;
+		}
+		ArrayList<String> ans=new ArrayList<>();
+		for(int i=0;i<N;i++){
+		    if(map.get(ma[i])==X){
+		        ans.add(name[i]);
+		    }
+		}
+		if(ans.size()==0){ System.out.print(-1); return;}
+		Collections.sort(ans);
+		for(String i:ans)
+		System.out.println(i);
+	}
+}
