96f4dfabf748015a1440d06fc311384573857152
Krishna K <krish1826s@gmail.com>
2024-06-10 10:31:46 +0530

    Update day_53.java

--- day_53.java
+++ day_53.java
@@ -13,3 +13,26 @@ class Solution {
         
     }
 }
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+		//Sum First & Unit Digits
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		String[] in=new String[n];
+		for(int i=0;i<n;i++) in[i]=sc.next();
+		for(int i=0;i<n;i++){
+		    String fd=""+in[i].charAt(0);
+		    String ld=""+in[i].charAt(in[i].length()-1);
+		    int ls=0;
+		    int rs=0;
+		    for(int j=0;j<n;j++){
+		        if(j<i&&in[j].endsWith(fd)) ls+=Integer.parseInt(in[j]);
+		        else if(j>i&&in[j].endsWith(ld)) rs+=Integer.parseInt(in[j]);
+		    }
+		    System.out.println(ls+" "+rs);
+		}
+
+	}
+}
