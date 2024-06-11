8db8bad720f5a32a868c30596299cf4888fdbab2
Krishna K <krish1826s@gmail.com>
2024-06-11 21:09:13 +0530

    Update day_54.java

--- day_54.java
+++ day_54.java
@@ -34,3 +34,35 @@ public class Hello {
 
 	}
 }
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+		//Sort based on Weight;
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int[] ar=new int[n];
+		int[] we=new int[n];
+		int max=0;
+		for(int i=0;i<n;i++){
+		    int nu=sc.nextInt();
+		    ar[i]=nu;
+		    if(nu%4==0&&nu%6==0) we[i]+=4;
+		    if(nu%2==0) we[i]+=3;
+		    if(((int)Math.sqrt(nu)*(int)Math.sqrt(nu))==nu) we[i]+=5;
+		    max=Math.max(max,we[i]);
+		}
+		List<String> ans =new ArrayList<>();
+		for(int i=0;i<=max;i++){
+		    for(int j=0;j<n;j++){
+		    if(we[j]==i)
+		    ans.add(ar[j]+" "+we[j]);
+		}
+		    
+		}
+		for(String i:ans){
+		    System.out.print(i);
+		}
+
+	}
+}
