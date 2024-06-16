cbdcc2e0e1f4290151f9f1e84baf2488a6a641ff
Krishna K <krish1826s@gmail.com>
2024-06-16 10:52:46 +0530

    Update day_59.java

--- day_59.java
+++ day_59.java
@@ -1,1 +1,31 @@
 
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+		//Maximum Coins -Seven dayss..
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int cou=0;
+		int c=a;
+		while(cou!=7){
+		    if(b==7){
+		        c*=2;
+		        b=1;
+		    }
+		    else if(b==1){
+		        c*=2;
+		        b=2;
+		    }
+		    else{
+		        c+=5;
+		        b++;
+		    }
+		    cou++;
+		}
+		System.out.print(c+"");
+		
+
+	}
+}
